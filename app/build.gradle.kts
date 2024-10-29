b87b32ead229541da634d5bc87d1d2d410842310
Piotr Pytlik <piotr.pytlik@euvic.pl>
2024-10-29 16:02:46 +0100

    Bump compose compiler and kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,12 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
 }
 
 dependencies {
@@ -44,7 +50,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    
+    implementation(libs.androidx.compose.navigation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
